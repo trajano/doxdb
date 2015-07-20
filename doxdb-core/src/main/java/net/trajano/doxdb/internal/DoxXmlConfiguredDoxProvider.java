bda86a036ec59194fbaeb8a5db0f28a9b9218b2b
Archimedes Trajano <Archimedes.Trajano@ontario.ca>
2015-07-20 17:34:38 -0400

    wip

--- doxdb-core/src/main/java/net/trajano/doxdb/internal/DoxXmlConfiguredDoxProvider.java
+++ doxdb-core/src/main/java/net/trajano/doxdb/internal/DoxXmlConfiguredDoxProvider.java
@@ -1,6 +1,13 @@
 package net.trajano.doxdb.internal;
 
+import java.sql.SQLException;
 import java.util.Map;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.PersistenceException;
+import javax.sql.DataSource;
 
 import net.trajano.doxdb.DoxFactory;
 import net.trajano.doxdb.jdbc.JdbcDoxFactory;
@@ -8,7 +15,8 @@ import net.trajano.doxdb.spi.DoxProvider;
 
 /**
  * This will read the Dox configuration from META-INF/dox.xml of the current
- * class loader.
+ * class loader. It is also responsible for getting the JDBC connection from the
+ * default data source.
  *
  * @author Archimedes
  */
@@ -16,7 +24,14 @@ public class DoxXmlConfiguredDoxProvider implements DoxProvider {
 
   @Override
   public DoxFactory createDoxFactory(String name, Map<String, String> options) {
-    return new JdbcDoxFactory();
+    try {
+      Context ctx = new InitialContext();
+      DataSource dataSource = (DataSource) ctx.lookup("java:comp/DefaultDataSource");
+      String[] doxNames = { "Sample" };
+      return new JdbcDoxFactory(dataSource.getConnection(), doxNames);
+    } catch (SQLException | NamingException e) {
+      throw new PersistenceException(e);
+    }
   }
 
 }
--- doxdb-core/src/main/java/net/trajano/doxdb/jdbc/JdbcDoxFactory.java
+++ doxdb-core/src/main/java/net/trajano/doxdb/jdbc/JdbcDoxFactory.java
@@ -1,20 +1,44 @@
 package net.trajano.doxdb.jdbc;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.ibm.jbatch.container.exception.PersistenceException;
+
+import net.trajano.doxdb.DoxConfiguration;
 import net.trajano.doxdb.DoxDAO;
 import net.trajano.doxdb.DoxFactory;
 
 public class JdbcDoxFactory implements DoxFactory {
 
+  final Map<String, JdbcDoxDAO> doxen = new ConcurrentHashMap<>();
+
+  private final Connection c;
+
+  public JdbcDoxFactory(Connection c, String... doxNames) {
+    this.c = c;
+    for (String doxName : doxNames) {
+      DoxConfiguration configuration = new DoxConfiguration();
+      configuration.setTableName(doxName);
+      configuration.setHasOob(true);
+      doxen.put(doxName, new JdbcDoxDAO(c, configuration));
+    }
+  }
+
   @Override
   public DoxDAO getDox(String name) {
-    // TODO Auto-generated method stub
-    return null;
+    return doxen.get(name);
   }
 
   @Override
   public void close() {
-    // TODO Auto-generated method stub
-
+    try {
+      c.close();
+    } catch (SQLException e) {
+      throw new PersistenceException(e);
+    }
   }
 
 }
