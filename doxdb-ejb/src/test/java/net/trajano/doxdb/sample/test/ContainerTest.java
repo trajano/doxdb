de9be30e6ce73496fd0981d4fc8e343f9ad8f8bc
Archimedes Trajano <Archimedes.Trajano@ontario.ca>
2015-07-24 17:42:35 -0400

    Fixed syntax errors

--- doxdb-ejb/src/test/java/net/trajano/doxdb/sample/test/ContainerTest.java
+++ doxdb-ejb/src/test/java/net/trajano/doxdb/sample/test/ContainerTest.java
@@ -1,31 +1,24 @@
 package net.trajano.doxdb.sample.test;
 
-import java.io.StringReader;
-
 import javax.ejb.embeddable.EJBContainer;
-import javax.json.Json;
-import javax.json.JsonObject;
 import javax.naming.Context;
 
 import org.junit.Test;
 
-import net.trajano.doxdb.ejb.SampleJsonBean;
-import net.trajano.doxdb.jdbc.DoxPrincipal;
+import net.trajano.doxdb.Dox;
 
 public class ContainerTest {
 
     @Test
     public void testCrud() throws Exception {
 
-        EJBContainer container = EJBContainer.createEJBContainer();
+        final EJBContainer container = EJBContainer.createEJBContainer();
         System.out.println(container);
-        Context context = container.getContext();
+        final Context context = container.getContext();
         System.out.println(context);
-        SampleJsonBean b = (SampleJsonBean) context.lookup("java:global/classes/SampleJsonBean");
-        String inputJson = "{\"name\":\"abc\"}";
-        JsonObject o = Json.createReader(new StringReader(inputJson))
-                .readObject();
-        System.out.println(b.create(o, new DoxPrincipal("PRINCE")));
+        final Dox b = (Dox) context.lookup("java:global/classes/JsonDox");
+        final String inputJson = "{\"name\":\"abc\"}";
+        System.out.println(b.create("collection", inputJson));
 
     }
 }
