e1444b0b9b5da42ca2daf083196a145278ada863
Archimedes Trajano <Archimedes.Trajano@ontario.ca>
2015-07-27 12:14:33 -0400

    Used web.xml to do the mapping

--- doxdb-rest/src/main/java/net/trajano/doxdb/rest/internal/DoxV1.java
+++
@@ -1,9 +0,0 @@
-package net.trajano.doxdb.rest.internal;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/V1")
-public class DoxV1 extends Application {
-
-}
