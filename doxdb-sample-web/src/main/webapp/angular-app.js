f6792b88c1c2a06d2e2f35a1f8f45cc9aa5db512
Archimedes Trajano <Archimedes.Trajano@ontario.ca>
2015-07-29 14:18:47 -0400

    Prevented reloading list after add

--- doxdb-sample-web/src/main/webapp/angular-app.js
+++ doxdb-sample-web/src/main/webapp/angular-app.js
@@ -8,22 +8,24 @@ angular.module('todoApp', [
     });
 
     var todoList = this;
-    function reloadVenues() {
 
-        Venue.query({}, function(venues) {
+    Venue.query({}, function(venues) {
 
-            todoList.venues = venues;
-            if (venues.length > 0) {
-                todoList.venue = venues[0];
-            }
-        });
-    }
-    reloadVenues();
+        todoList.venues = venues;
+        if (venues.length > 0) {
+            todoList.venue = venues[0];
+        }
+    });
+
     todoList.saveVenue = function() {
 
         if (!todoList.venue._id) {
 
-            new Venue(todoList.venue).$save().then(reloadVenues);
+            new Venue(todoList.venue).$save().then(function(venue) {
+
+                todoList.venue = venue;
+                todoList.venues.push(venue);
+            });
         } else {
             todoList.venue.$save().then(function(venue) {
 
