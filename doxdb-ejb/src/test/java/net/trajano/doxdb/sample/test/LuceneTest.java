77263a4e5765237ed686701978af69705071de4a
Archimedes Trajano <arch@trajano.net>
2015-07-30 09:16:44 -0400

    Added load testing data

--- doxdb-ejb/src/test/java/net/trajano/doxdb/sample/test/LuceneTest.java
+++ doxdb-ejb/src/test/java/net/trajano/doxdb/sample/test/LuceneTest.java
@@ -2,11 +2,22 @@ package net.trajano.doxdb.sample.test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Map.Entry;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonString;
+import javax.json.JsonValue;
+
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
@@ -14,8 +25,11 @@ import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.NIOFSDirectory;
 import org.junit.Test;
 
+import net.trajano.commons.testing.ResourceUtil;
 import net.trajano.doxdb.search.lucene.JpaDirectory;
 
 /**
@@ -24,6 +38,95 @@ import net.trajano.doxdb.search.lucene.JpaDirectory;
  * @author Archimedes
  */
 public class LuceneTest extends AbstractBeanTest {
+
+    @Test
+    public void testLargeLucene() throws Exception {
+
+        tx.begin();
+        {
+            final Analyzer analyzer = new StandardAnalyzer();
+            final IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+            final JpaDirectory dir = new JpaDirectory(em, "searchtable");
+            final IndexWriter indexWriter = new IndexWriter(dir, iwc);
+
+            final InputStream resourceAsStream = ResourceUtil.getResourceAsStream("MOCK_DATA.json");
+            final JsonArray collection = Json.createReader(resourceAsStream).readArray();
+
+            for (final JsonValue item : collection) {
+                final JsonObject record = (JsonObject) item;
+
+                final Document doc = new Document();
+                for (final Entry<String, JsonValue> field : record.entrySet()) {
+                    if (field.getValue() instanceof JsonString) {
+                        doc.add(new StringField(field.getKey(), ((JsonString) field.getValue()).getString(), Store.YES));
+                    }
+                }
+                indexWriter.addDocument(doc);
+            }
+
+            indexWriter.close();
+        }
+        tx.commit();
+        tx.begin();
+        {
+
+            final JpaDirectory dir = new JpaDirectory(em, "searchtable");
+            final IndexSearcher searcher = new IndexSearcher(DirectoryReader.open(dir));
+            final Analyzer analyzer = new StandardAnalyzer();
+            final QueryParser parser = new QueryParser("name", analyzer);
+            final Query query = parser.parse("value");
+            final TopDocs search = searcher.search(query, 10);
+            assertEquals(1, search.totalHits);
+            final Document doc = searcher.doc(search.scoreDocs[0].doc);
+            assertEquals("value", doc.get("name"));
+        }
+        tx.commit();
+    }
+
+    @Test
+    public void testLargeLucene2() throws Exception {
+
+        final Path path = testFolder.getRoot().toPath();
+
+        {
+            final Analyzer analyzer = new StandardAnalyzer();
+            final IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+            final Directory dir = new NIOFSDirectory(path);
+            final IndexWriter indexWriter = new IndexWriter(dir, iwc);
+
+            final InputStream resourceAsStream = ResourceUtil.getResourceAsStream("MOCK_DATA.json");
+            final JsonArray collection = Json.createReader(resourceAsStream).readArray();
+
+            for (final JsonValue item : collection) {
+                final JsonObject record = (JsonObject) item;
+
+                final Document doc = new Document();
+                for (final Entry<String, JsonValue> field : record.entrySet()) {
+
+                    if (field.getValue() instanceof JsonString) {
+                        doc.add(new TextField(field.getKey(), ((JsonString) field.getValue()).getString(), Store.YES));
+                    }
+                }
+                indexWriter.addDocument(doc);
+            }
+
+            indexWriter.close();
+            dir.close();
+        }
+
+        {
+
+            final Directory dir = new NIOFSDirectory(path);
+            final IndexSearcher searcher = new IndexSearcher(DirectoryReader.open(dir));
+            final Analyzer analyzer = new StandardAnalyzer();
+            final QueryParser parser = new QueryParser("last_name", analyzer);
+            final Query query = parser.parse("Trajano");
+            final TopDocs search = searcher.search(query, 10);
+            assertEquals(1, search.totalHits);
+            final Document doc = searcher.doc(search.scoreDocs[0].doc);
+            assertEquals("Archimedes", doc.get("first_name"));
+        }
+    }
 
     @Test
     public void testLucene() throws Exception {
@@ -41,6 +144,7 @@ public class LuceneTest extends AbstractBeanTest {
             indexWriter.addDocument(doc);
             indexWriter.commit();
             indexWriter.close();
+            dir.close();
         }
         {
 
---
+++ doxdb-sample-ejb/src/test/java/net/trajano/doxdb/sample/test/DataLoader.java
@@ -0,0 +1,37 @@
+package net.trajano.doxdb.sample.test;
+
+import java.io.InputStream;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonValue;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.core.MediaType;
+
+import net.trajano.commons.testing.ResourceUtil;
+
+/**
+ * This will call the REST API and load MOCK_DATA.json.
+ *
+ * @author Archimedes Trajano
+ */
+public class DataLoader {
+
+    public static void main(final String[] args) {
+
+        final InputStream resourceAsStream = ResourceUtil.getResourceAsStream("MOCK_DATA.json");
+        final JsonArray collection = Json.createReader(resourceAsStream).readArray();
+        final Builder request = ClientBuilder.newClient().target("http://localhost:8080/doxdb-sample/V1/venue").request(MediaType.APPLICATION_JSON_TYPE);
+
+        for (final JsonValue item : collection) {
+            final JsonObject record = (JsonObject) item;
+            final JsonObject jsonObject = Json.createObjectBuilder().add("name", record.get("last_name")).add("language", record.get("language")).build();
+
+            request.post(Entity.entity(jsonObject, MediaType.APPLICATION_JSON_TYPE));
+        }
+
+    }
+}
