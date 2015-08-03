f2454518039ee9591a5f154169e4fbeb369bde03
Archimedes Trajano <arch@trajano.net>
2015-08-02 22:29:47 -0400

    added missing semicolon

--- doxdb-sample-web/src/main/webapp/search.js
+++ doxdb-sample-web/src/main/webapp/search.js
@@ -1,24 +1,27 @@
-angular.module('search', [ 'ngResource' ]).controller('SearchController',
-		function($resource) {
+angular.module('search', [
+    'ngResource'
+]).controller('SearchController', function($resource) {
 
-			var MyIndex = $resource('V1/search/myindex', null, {
-				'search' : {
-					'method' : 'GET'
-				}
-			});
+    var MyIndex = $resource('V1/search/myindex', null, {
+        'search' : {
+            'method' : 'GET'
+        }
+    });
 
-			var search = this;
+    var search = this;
 
-			search.query = '';
-			search.result = {};
-			search.doSearch = function() {
-				if (search.query) {
-					MyIndex.search({
-						'q' : search.query
-					}, function(result) {
-						search.result = result;
-					});
-				}
-			}
+    search.query = '';
+    search.result = {};
+    search.doSearch = function() {
 
-		});
+        if (search.query) {
+            MyIndex.search({
+                'q' : search.query
+            }, function(result) {
+
+                search.result = result;
+            });
+        }
+    };
+
+});
