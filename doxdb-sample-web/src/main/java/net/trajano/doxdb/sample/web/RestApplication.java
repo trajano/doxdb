cbb39d2bd519b368f66c7f69f7fea7a4cd01006e
Archimedes Trajano <arch@trajano.net>
2015-08-13 05:58:59 -0400

    Fix for WildFly
    WildFly does not work with javax.ws.rs.core.Application as the servlet
    name.

---
+++ doxdb-sample-web/src/main/java/net/trajano/doxdb/sample/web/RestApplication.java
@@ -0,0 +1,13 @@
+package net.trajano.doxdb.sample.web;
+
+import javax.ws.rs.ApplicationPath;
+
+/**
+ * type.
+ *
+ * @author Archimedes Trajano
+ */
+@ApplicationPath("api")
+public class RestApplication {
+
+}
