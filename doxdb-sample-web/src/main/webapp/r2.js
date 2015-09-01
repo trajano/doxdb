147b733b6f12a0dcb5b610c836e48b72c833ddc1
Archimedes Trajano <Archimedes.Trajano@ontario.ca>
2015-09-01 18:06:22 -0400

    routing added

--- doxdb-sample-web/src/main/webapp/r2.js
+++ doxdb-sample-web/src/main/webapp/r2.js
@@ -1,12 +1,46 @@
 angular.module('doxdbApp', [
-    'doxdb', 'ngRoute', 'ngMdIcons'
+    'doxdb', 'ngRoute'
 ])
+
+.config(function($routeProvider) {
+
+    $routeProvider
+
+    .when("/welcome", {
+        templateUrl : "partials/welcome.html",
+        controller : "DoxDBWelcomeController",
+        controllerAs : "doxdbWelcome"
+    })
+
+    .when("/:name", {
+        templateUrl : "partials/schema.html",
+        controller : "DoxDBSchemaController",
+        controllerAs : "doxdbSchema"
+    })
+
+    .otherwise("/welcome");
+
+})
 
 .controller('DoxDBNavController', function(DoxDBService) {
 
     navController = this;
 
     navController.schemas = DoxDBService.schemas;
+})
+
+.controller('DoxDBSchemaController', function(DoxDBService, $routeParams, $log) {
+
+    schemaController = this;
+    $log.info($routeParams);
+    $log.info("HERE");
+    schemaController.schema = $routeParams.name;
+})
+
+.controller('DoxDBWelcomeController', function(DoxDBService) {
+
+    welcomeController = this;
+
 })
 
 .service('DoxDBService', function() {
